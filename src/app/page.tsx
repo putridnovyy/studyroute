3905668bda92fa8b9f6b3d772879714001a843b6
putridnovy <putridnovyy@upi.edu>
2023-09-24 05:04:48 +0700

    Create Course Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button>
-      <h1 className="text-zinc-200">Click Me</h1>
+    <Button className="dark:bg-gray-950">
+      <h1 className="text-zinc-200 dark:border-white mt-4">Click Me</h1>
     </Button>
   )
 }
